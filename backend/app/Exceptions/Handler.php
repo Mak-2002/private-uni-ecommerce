7edf01f5a8d27d543881d24968189e8ef5249122
Qusai Nasr <kossay.ar.en@gmail.com>
2023-08-22 03:29:41 +0300

    Implement User Auth
    Implemented user authentication (register, login and logout)

--- backend/app/Exceptions/Handler.php
+++ backend/app/Exceptions/Handler.php
@@ -2,7 +2,10 @@
 
 namespace App\Exceptions;
 
+use GuzzleHttp\Psr7\Request;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Support\Str;
+use Illuminate\Validation\ValidationException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -26,5 +29,43 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        $file = Str::replace(base_path(), '', $exception->getFile());
+        $data = [
+            'success' => false,
+            'message' => $exception->getMessage() . " in Line " . $exception->getLine() . ", File: " . $file,
+        ];
+
+        if ($exception instanceof ValidationException)
+            $data['errors'] = $exception->validator->errors()->getMessages();
+
+        return response()->json($data, $this->getStatusCodeForException($exception));
+    }
+
+    protected function getStatusCodeForException(Throwable $exception)
+    {
+        if ($exception instanceof \Illuminate\Database\Eloquent\ModelNotFoundException)
+            return 404;
+
+        elseif ($exception instanceof \Illuminate\Validation\ValidationException)
+            return 422;
+
+        elseif ($exception instanceof \Symfony\Component\HttpKernel\Exception\NotFoundHttpException)
+            return 404;
+
+        elseif ($exception instanceof \Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException)
+            return 405;
+
+        elseif ($exception instanceof \Illuminate\Auth\Access\AuthorizationException)
+            return 403;
+
+        elseif ($exception instanceof \Illuminate\Database\QueryException)
+            return 500;
+
+        else
+            return 500;
     }
 }
---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    protected function attemptOrFail(Request $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password')))
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email|max:255',
+            'password' => 'required|min:8|max:255',
+        ]);
+
+        $this->attemptOrFail($request);
+        return response()->json([
+            'success' => true,
+            'auth_token' => Auth::user()->createToken('auth_token')->plainTextToken,
+        ], 200);
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            //DEBUG:commented 'name' => 'required|string|max:255|/^[a-zA-Z]+(?:\s[a-zA-Z]+){0,2}$/',
+            'name' => 'required|string|max:255', //DEBUG:wrote
+            'email' => 'required|string|email|unique:users|max:255',
+            'password' => 'required|min:8|max:255|confirmed'
+        ]);
+
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $this->attemptOrFail($request);
+        return response()->json([
+            'success' => true,
+            'auth_token' => Auth::user()->createToken('auth_token')->plainTextToken,
+        ], 201);
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+    }
+}
