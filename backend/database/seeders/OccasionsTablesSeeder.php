5602230993fd05c188c46b09f9c18cd0a3bfee5a
AmeerHz <ameer.hamze9@gmail.com>
2024-02-24 15:55:39 +0300

    Update Product Seeding

--- backend/database/seeders/OccasionsTablesSeeder.php
+++ backend/database/seeders/OccasionsTablesSeeder.php
@@ -23,16 +23,24 @@ class OccasionsTablesSeeder extends Seeder
                 'name' => $occasionName,
             ]);
 
+        $graduationCeremonyProducts = [
+            1, 2, 3, 7, 60, 74
+        ];
+
+        $birthdayPartyProducts = [
+            4, 5, 6, 8, 9, 11, 77, 60, 74, 58, 55, 54
+        ];
+
         // Add product suggestions to occasions
-        for ($i = 1; $i < 4; $i++) {
+        foreach ($graduationCeremonyProducts as $productId)
             OccasionProduct::create([
                 'occasion_id' => 1,
-                'product_id' => $i,
+                'product_id' => $productId,
             ]);
+        foreach ($birthdayPartyProducts as $productId)
             OccasionProduct::create([
                 'occasion_id' => 2,
-                'product_id' => $i + 3,
+                'product_id' => $productId,
             ]);
-        }
     }
 }
